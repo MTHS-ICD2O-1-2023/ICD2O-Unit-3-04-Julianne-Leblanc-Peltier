88b95eb2454bbfe87027c6f6fee46c689c5dbcdb
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-10 18:01:11 +0000

    added all html css + js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const temperatureInFahrenheit = parseFloat(document.getElementById("temperature-in-fahrenheit").value)
+
+  // process
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) * (5.0 / 9.0)
+
+  // output
+  document.getElementById("temperature-in-celsius").innerHTML = temperatureInCelsius.toFixed(3) + "°C"
+}
